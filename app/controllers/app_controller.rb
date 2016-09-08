459caa876bf1d86e186c327a542809153f343e2e
Shi Hao <shihao@piktochart.com>
2016-09-08 11:02:11 +0800

    [TASK] Completed team OKR details pages [VIEW only]

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -34,13 +34,13 @@ class AppController < ApplicationController
 
       @temp_team_objective.each_with_index do |item,index|
         @count = 0
-          while(@count < item.count)
-            @team_key_results = TeamKeyResult.where(team_objective_id: item[@count][1]).all.map{|kr| [kr.key_result, kr.id]}
-            @team_key_results.each do |element|
-              @temp_team_key_result.push(element)
-            end
-            @count = @count + 1
-          end      
+        while(@count < item.count)
+          @team_key_results = TeamKeyResult.where(team_objective_id: item[@count][1]).all.map{|kr| [kr.key_result, kr.id]}
+          @team_key_results.each do |element|
+            @temp_team_key_result.push(element)
+          end
+          @count = @count + 1
+        end      
       end
 
       # @team_key_results = TeamKeyResult.where(team_objective_id: @team_objectives)
