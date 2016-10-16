45bdcc4e1eba79c379c8d36ce5b26453af385703
Shi Hao <shihao@piktochart.com>
2016-10-16 11:07:18 +0800

    [TASK] Declared pre-define data required for the system to run at start.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# --------
+# Okr Role 
+# --------
+
+OkrRole.create!(name: "Admin", description: "Admin is the one who controls everything in the system.")
+OkrRole.create!(name: "Team Lead", description: "Team Lead manages the team.")
+OkrRole.create!(name: "Employee", description: "Employee is the basic unit of a company.")
+
+# ------------------------
+# Security and Role Module 
+# ------------------------
+
+# Control Type 
+
+company_okr_control = ControlType.create!(okr_system_type: "Company OKR")
+team_okr_control = ControlType.create!(okr_system_type: "Team OKR")
+user_control = ControlType.create!(okr_system_type: "User Admin")
+team_control = ControlType.create!(okr_system_type: "Team Admin")
+timeframe_control = ControlType.create!(okr_system_type: "Timeframe Control")
+
+# Controls
+
+Control.create!(details: "Create Company OKR", control_type_id: company_okr_control.id)
+Control.create!(details: "Edit Company OKR", control_type_id: company_okr_control.id)
+Control.create!(details: "Delete Company OKR", control_type_id: company_okr_control.id)
+Control.create!(details: "Create Team OKR", control_type_id: team_okr_control.id)
+Control.create!(details: "Edit Team OKR", control_type_id: team_okr_control.id)
+Control.create!(details: "Delete Team OKR", control_type_id: team_okr_control.id)
+Control.create!(details: "Create User", control_type_id: user_control.id)
+Control.create!(details: "Edit User Information", control_type_id: user_control.id)
+Control.create!(details: "Delete User", control_type_id: user_control.id)
+Control.create!(details: "View System Users Information", control_type_id: user_control.id)
+Control.create!(details: "Create Team", control_type_id: team_control.id)
+Control.create!(details: "Edit Team Information", control_type_id: team_control.id)
+Control.create!(details: "Delete Team", control_type_id: team_control.id)
+Control.create!(details: "View System Teams Information", control_type_id: team_control.id)
+Control.create!(details: "Set time interval for particular year", control_type_id: timeframe_control.id)
+Control.create!(details: "View System Timeframes", control_type_id: timeframe_control.id)
+
+
